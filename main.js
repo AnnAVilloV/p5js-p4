91fec3685a849757f8cec0a1e49d7194c3ed4fe6
AnnAVilloV <jyliu435@gmail.com>
2023-05-01 22:12:12 +0100

    add

--- main.js
+++ main.js
@@ -2,9 +2,59 @@ let state = 0;
 let fishimage;
 let anglerimage;
 let bgpic;
+let gfb;
+let eel1;
+let eel2;
+let eel3;
+let eel4;
+
 
 function setup(){
     // createCanvas(400, 400);
+    setupPic();
+
+
+    port = createSerial();
+    let usedPorts = usedSerialPorts();
+    if (usedPorts.length > 0) {
+      port.open(usedPorts[0], 115200);
+    }
+    // any other ports can be opened via a dialog after
+    // user interaction (see connectBtnClick below)
+    connectBtn = createButton('Connect to Micro:bit');
+    connectBtn.position(80, 200);
+    connectBtn.mousePressed(connectBtnClick);
+
+    let sendBtn = createButton('Send hello');
+    sendBtn.position(220, 200);
+    sendBtn.mousePressed(sendBtnClick);
+}
+
+
+function draw(){
+    //state = 0,连接; state = 1, 主界面
+    if(state == 0){
+
+    }else if(state == 1){
+        drawPic();
+        textData.forEach((el, i) => {
+            fill(255);
+            textSize(10);
+            text(el, 20, 80 + i * 20);
+            });
+    }
+
+
+
+    
+
+}
+
+function setupPort(){
+    
+}
+
+function setupPic(){
     createCanvas(displayWidth, displayHeight);
     fishimage = loadImage("assets/fish.png");
     anglerimage = loadImage("assets/anglerfish.png");
@@ -17,13 +67,8 @@ function setup(){
 }
 
 
-function draw(){
-    //state = 0,连接; state = 1, 主界面
-    // if(state == 0){
 
-    // }else if(state == 1){
-
-    // }
+function drawPic(){
     image(bgpic,0,0,displayWidth,displayHeight);
     image(anglerimage,displayWidth*0.55, displayHeight*0.3, anglerimage.width*0.8, anglerimage.height*0.8);
     image(gfb,displayWidth*0.85, displayHeight*0.05,gfb.width*0.12,gfb.height*0.12);
@@ -40,8 +85,4 @@ function draw(){
     text("Inside Temperature",displayWidth*0.2, displayHeight*0.8);
     text("Outside Humidity",displayWidth*0.3, displayHeight*0.8);
     text("Inside Humidity",displayWidth*0.4, displayHeight*0.8);
-
-
-    
-
 }
